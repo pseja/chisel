09bfd3f49d8876bd5bca500e01e4af6d28705c7f
Lukas Pseja <luk4sps3j4@gmail.com>
2025-08-12 13:42:46 +0200

    setup

---
+++ src/main/java/io/github/pseja/chisel/ChiselClient.java
@@ -0,0 +1,10 @@
+package io.github.pseja.chisel;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChiselClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
