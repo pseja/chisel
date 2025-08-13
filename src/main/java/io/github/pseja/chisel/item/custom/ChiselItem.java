2aeafc022e96d46aba4c05f76a50695c7999fdf8
Lukas Pseja <luk4sps3j4@gmail.com>
2025-08-13 15:15:59 +0200

    chisel can be enchanted

--- src/main/java/io/github/pseja/chisel/item/custom/ChiselItem.java
+++ src/main/java/io/github/pseja/chisel/item/custom/ChiselItem.java
@@ -1,9 +1,15 @@
 package io.github.pseja.chisel.item.custom;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class ChiselItem extends Item {
     public ChiselItem(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public boolean isEnchantable(ItemStack stack) {
+        return super.isEnchantable(stack);
     }
 }
