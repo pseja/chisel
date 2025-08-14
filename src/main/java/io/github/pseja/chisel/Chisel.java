2c19f2e0deb1106cfbb2a0d16b0514d5ce861643
Lukas Pseja <luk4sps3j4@gmail.com>
2025-08-14 15:45:38 +0200

    remove unused stuff

--- src/main/java/io/github/pseja/chisel/Chisel.java
+++ src/main/java/io/github/pseja/chisel/Chisel.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 
 public class Chisel implements ModInitializer {
     public static final String MOD_ID = "chisel";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
